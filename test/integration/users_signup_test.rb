e26fe883bdac45244640e793cddfeff5dc6d33d4
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-11-18 20:22:19 -0600

    valid submissions work, more integration testing

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,4 +13,30 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       end
       assert_template 'users/new'
   end
+
+  test 'error messages appear on invalid submission' do
+    get signup_path
+    assert_no_difference  'User.count' do
+      post users_path, params: {user: {
+          name: '',
+          email: 'user@invalid',
+          password: '123',
+          password_confirmation: '456'
+        }}
+      end
+      assert_template 'users/new'
+      assert_select 'div#error_explanation'
+  end
+
+  test 'new item in db if user signup is successful' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: {user: {
+          name: 'good user',
+          email: 'user@valid.com',
+          password: '123456',
+          password_confirmation: '123456'
+        }}
+    end
+  end
 end
