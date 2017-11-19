01c8ccaa94d8555a0be6f20ead828d50ea2ee187
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-11-18 19:48:41 -0600

    added gravatar for user

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
 
   get '/contact', to: 'static_pages#contact'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+
+  resources :users
+
+
 end
