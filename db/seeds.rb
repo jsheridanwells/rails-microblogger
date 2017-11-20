37eb7ac1b087028c61343bffc8375bc773750f8b
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-11-19 19:54:17 -0600

    delete users tests pass

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "123456",
+             password_confirmation: "123456",
+             admin: true)
+
 99.times do |num|
   name = Faker::Name.unique.first_name
   email = "#{name}@example.com"
